d79d588cbfe196303c52e39b6c9422d5e6f96796
Mat Wood <mat@thepacketgeek.com>
2014-12-05 20:09:38 -0800

    Adding IPv6 Generator
    With sequential, mask, random support.

--- sanicap/sanicap.py
+++ sanicap/sanicap.py
@@ -79,7 +79,7 @@ class MACGenerator(object):
             self.mappings[address] = self._next_mac(address)
             return self.mappings[address]
 
-class IPGenerator(object):
+class IPv4Generator(object):
     def __init__(self, start_ip='10.0.0.1', sequential=True, mask=0):
         self.start_ip = self._last_ip = start_ip
         self.started = False
@@ -89,10 +89,9 @@ class IPGenerator(object):
     
     def _increment(self, address):
 
-        version = 4 #ipaddress.ip_address(self._last_ip).version
         #pad binary number first so it's the correct length
         def pad_bin(unpadded):
-            return format(int(ipaddress.ip_address(unicode(unpadded))), '032b')
+            return format(int(ipaddress.IPv4Address(unicode(unpadded))), '032b')
 
         ip_bin = pad_bin(self._last_ip)
         
@@ -112,7 +111,7 @@ class IPGenerator(object):
     def _random_ip(self, address):
 
         def pad_bin(unpadded):
-            return format(int(ipaddress.ip_address(unicode(unpadded))), '032b')
+            return format(int(ipaddress.IPv4Address(unicode(unpadded))), '032b')
 
         unmasked = ''.join([str(randint(0,1)) for x in xrange(0, 32 - self.mask)])
 
@@ -123,6 +122,70 @@ class IPGenerator(object):
             full_bin = pad_bin(address)[:self.mask] + unmasked
 
         return str(ipaddress.IPv4Address(int(full_bin, 2)))
+
+    def _next_ip(self, address):
+
+        if self.sequential:
+            self._last_ip = self._increment(address)
+        else:
+            self._last_ip = self._random_ip(address)
+
+        if self._last_ip not in self.mappings.itervalues():
+            return self._last_ip
+        else:
+            return self._next_ip(address)
+
+    def get_ip(self, address):
+        # check address mapping
+        try:
+            return self.mappings[address]
+        except KeyError:
+            self.mappings[address] = self._next_ip(address)
+            return self.mappings[address]
+
+class IPv6Generator(object):
+    def __init__(self, start_ip='2001::1', sequential=True, mask=0):
+        self.start_ip = self._last_ip = start_ip
+        self.started = False
+        self.mappings = {}
+        self.sequential = sequential
+        self.mask = mask
+    
+    def _increment(self, address):
+
+        #pad binary number first so it's the correct length
+        def pad_bin(unpadded):
+            return format(int(ipaddress.IPv6Address(unicode(unpadded))), '0128b')
+
+        ip_bin = pad_bin(self._last_ip)
+        
+        #check to make sure we haven't hit highest number in mask (and wrapped back to 0)
+        if '0' not in ip_bin[self.mask:]:
+            raise OverflowError('Ran out of IP addresses, try a smaller mask or lower starting IP.')
+        
+        #only increment if it's not the first iteration
+        if self.started:
+            full_bin = pad_bin(address)[:self.mask] + format(int(ip_bin[self.mask:], 2) + 1, '0' + str(128 - self.mask) + 'b')
+        else:
+            self.started = True
+            full_bin = pad_bin(address)[:self.mask] + format(int(ip_bin[self.mask:], 2), '0' + str(128 - self.mask) + 'b')
+        
+        return str(ipaddress.IPv6Address(int(full_bin, 2)))
+
+    def _random_ip(self, address):
+
+        def pad_bin(unpadded):
+            return format(int(ipaddress.IPv6Address(unicode(unpadded))), '0128b')
+
+        unmasked = ''.join([str(randint(0,1)) for x in xrange(0, 128 - self.mask)])
+
+        if self.started:
+            full_bin = pad_bin(address)[:self.mask] + unmasked
+        else:
+            self.started = True
+            full_bin = pad_bin(address)[:self.mask] + unmasked
+
+        return str(ipaddress.IPv6Address(int(full_bin, 2)))
 
     def _next_ip(self, address):
 
