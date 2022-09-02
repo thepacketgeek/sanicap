fe631d2537c88aba144b59024dde6054f2afc331
Simeon Miteff <simeon.miteff@corelight.com>
2022-09-02 11:56:35 +1000

    PEP8 reformat

--- sanicap/sanicap.py
+++ sanicap/sanicap.py
@@ -18,25 +18,25 @@ class MACGenerator(object):
         self.mappings = {}
         self.sequential = sequential
         self.mask = mask
-    
+
     def _increment(self, address):
 
-        #pad hex number first so it's the correct length
+        # pad hex number first so it's the correct length
         def pad_bin(unpadded):
-            return format(int('0x' + unpadded.replace(':','').replace('.',''), 16), '048b')
+            return format(int('0x' + unpadded.replace(':', '').replace('.', ''), 16), '048b')
 
         mac_bin = pad_bin(self._last_mac)
 
-        #check to make sure we haven't hit highest number in mask (and wrapped back to 0)
+        # check to make sure we haven't hit highest number in mask (and wrapped back to 0)
         if '0' not in mac_bin[self.mask:]:
             raise OverflowError('Ran out of MAC addresses, try a smaller mask or lower starting MAC.')
-    
-        #only increment if it's not the first iteration
+
+        # only increment if it's not the first iteration
         if self.started:
             if self.mask > 0:
-                masked = format(int(pad_bin(address)[:self.mask], 2), '0'+ str(self.mask) +'b')
-                unmasked = format(int(mac_bin[self.mask:], 2) + 1, '0'+ str(48 - self.mask) +'b')
-                returned_bin = format(int(masked + unmasked, 2) , '012x')
+                masked = format(int(pad_bin(address)[:self.mask], 2), '0' + str(self.mask) + 'b')
+                unmasked = format(int(mac_bin[self.mask:], 2) + 1, '0' + str(48 - self.mask) + 'b')
+                returned_bin = format(int(masked + unmasked, 2), '012x')
             else:
                 returned_bin = format(int(mac_bin, 2) + 1, '012x')
 
@@ -45,21 +45,21 @@ class MACGenerator(object):
             if self.mask > 0:
                 masked = format(int(pad_bin(address)[:self.mask], 2), '0%sb' % str(self.mask))
                 unmasked = format(int(mac_bin[self.mask:], 2), '0%sb' % str(48 - self.mask))
-                returned_bin = format(int(masked + unmasked, 2) , '012x')
+                returned_bin = format(int(masked + unmasked, 2), '012x')
             else:
                 returned_bin = format(int(mac_bin, 2), '012x')
-        
+
         return ':'.join(textwrap.wrap(returned_bin, 2))
 
     def _random_mac(self, address):
 
         def pad_bin(unpadded):
-            return format(int('0x' + unpadded.replace(':','').replace('.',''), 16), '048b')
+            return format(int('0x' + unpadded.replace(':', '').replace('.', ''), 16), '048b')
 
-        unmasked = ''.join(str(randint(0,1)) for x in range(0, 48 - self.mask))
+        unmasked = ''.join(str(randint(0, 1)) for x in range(0, 48 - self.mask))
 
         full_bin = pad_bin(address)[:self.mask] + unmasked
-        
+
         return ':'.join(textwrap.wrap(format(int(full_bin, 2), '012x'), 2))
 
     def _next_mac(self, address):
@@ -86,6 +86,7 @@ class MACGenerator(object):
         else:
             return address
 
+
 class IPv4Generator(object):
     def __init__(self, start_ip, sequential, mask):
         self.start_ip = self._last_ip = start_ip
@@ -93,26 +94,28 @@ class IPv4Generator(object):
         self.mappings = {}
         self.sequential = sequential
         self.mask = mask
-    
+
     def _increment(self, address):
 
-        #pad binary number first so it's the correct length
+        # pad binary number first so it's the correct length
         def pad_bin(unpadded):
             return format(int(ipaddress.IPv4Address(str(unpadded))), '032b')
 
         ip_bin = pad_bin(self._last_ip)
-        
-        #check to make sure we haven't hit highest number in mask (and wrapped back to 0)
+
+        # check to make sure we haven't hit highest number in mask (and wrapped back to 0)
         if '0' not in ip_bin[self.mask:]:
             raise OverflowError('Ran out of IP addresses, try a smaller mask or lower starting IP.')
-        
-        #only increment if it's not the first iteration
+
+        # only increment if it's not the first iteration
         if self.started:
-            full_bin = pad_bin(address)[:self.mask] + format(int(ip_bin[self.mask:], 2) + 1, '0' + str(32 - self.mask) + 'b')
+            full_bin = pad_bin(address)[:self.mask] + format(int(ip_bin[self.mask:], 2) + 1,
+                                                             '0' + str(32 - self.mask) + 'b')
         else:
             self.started = True
-            full_bin = pad_bin(address)[:self.mask] + format(int(ip_bin[self.mask:], 2), '0' + str(32 - self.mask) + 'b')
-        
+            full_bin = pad_bin(address)[:self.mask] + format(int(ip_bin[self.mask:], 2),
+                                                             '0' + str(32 - self.mask) + 'b')
+
         return str(ipaddress.IPv4Address(int(full_bin, 2)))
 
     def _random_ip(self, address):
@@ -120,7 +123,7 @@ class IPv4Generator(object):
         def pad_bin(unpadded):
             return format(int(ipaddress.IPv4Address(str(unpadded))), '032b')
 
-        unmasked = ''.join(str(randint(0,1)) for x in range(0, 32 - self.mask))
+        unmasked = ''.join(str(randint(0, 1)) for x in range(0, 32 - self.mask))
 
         if self.started:
             full_bin = pad_bin(address)[:self.mask] + unmasked
@@ -154,6 +157,7 @@ class IPv4Generator(object):
         else:
             return address
 
+
 class IPv6Generator(object):
     def __init__(self, start_ip, sequential, mask):
         self.start_ip = self._last_ip = start_ip
@@ -161,26 +165,28 @@ class IPv6Generator(object):
         self.mappings = {}
         self.sequential = sequential
         self.mask = mask
-    
+
     def _increment(self, address):
 
-        #pad binary number first so it's the correct length
+        # pad binary number first so it's the correct length
         def pad_bin(unpadded):
             return format(int(ipaddress.IPv6Address(str(unpadded))), '0128b')
 
         ip_bin = pad_bin(self._last_ip)
-        
-        #check to make sure we haven't hit highest number in mask (and wrapped back to 0)
+
+        # check to make sure we haven't hit highest number in mask (and wrapped back to 0)
         if '0' not in ip_bin[self.mask:]:
             raise OverflowError('Ran out of IP addresses, try a smaller mask or lower starting IP.')
-        
-        #only increment if it's not the first iteration
+
+        # only increment if it's not the first iteration
         if self.started:
-            full_bin = pad_bin(address)[:self.mask] + format(int(ip_bin[self.mask:], 2) + 1, '0' + str(128 - self.mask) + 'b')
+            full_bin = pad_bin(address)[:self.mask] + format(int(ip_bin[self.mask:], 2) + 1,
+                                                             '0' + str(128 - self.mask) + 'b')
         else:
             self.started = True
-            full_bin = pad_bin(address)[:self.mask] + format(int(ip_bin[self.mask:], 2), '0' + str(128 - self.mask) + 'b')
-        
+            full_bin = pad_bin(address)[:self.mask] + format(int(ip_bin[self.mask:], 2),
+                                                             '0' + str(128 - self.mask) + 'b')
+
         return str(ipaddress.IPv6Address(int(full_bin, 2)))
 
     def _random_ip(self, address):
@@ -188,7 +194,7 @@ class IPv6Generator(object):
         def pad_bin(unpadded):
             return format(int(ipaddress.IPv6Address(str(unpadded))), '0128b')
 
-        unmasked = ''.join(str(randint(0,1)) for x in range(0, 128 - self.mask))
+        unmasked = ''.join(str(randint(0, 1)) for x in range(0, 128 - self.mask))
 
         if self.started:
             full_bin = pad_bin(address)[:self.mask] + unmasked
@@ -222,45 +228,45 @@ class IPv6Generator(object):
             return address
 
 
-def sanitize(filepath_in, *, filepath_out = None, sequential=True, append=False, ipv4_mask=0, ipv6_mask=0, mac_mask=0, start_ipv4='10.0.0.1', start_ipv6='2001:aa::1', start_mac='00:aa:00:00:00:01', fixed_vlan=None, info=True):
-
+def sanitize(filepath_in, *, filepath_out=None, sequential=True, append=False, ipv4_mask=0, ipv6_mask=0, mac_mask=0,
+             start_ipv4='10.0.0.1', start_ipv6='2001:aa::1', start_mac='00:aa:00:00:00:01', fixed_vlan=None, info=True):
     if not filepath_out:
         timestamp = datetime.datetime.now().strftime('%y%m%d-%H%m%S')
         filepath_out = os.path.splitext(filepath_in)[0] + '_sanitized_' + timestamp + os.path.splitext(filepath_in)[1]
-    
+
     mac_gen = MACGenerator(sequential=sequential, mask=mac_mask, start_mac=start_mac)
     ip4_gen = IPv4Generator(sequential=sequential, mask=ipv4_mask, start_ip=start_ipv4)
     ip6_gen = IPv6Generator(sequential=sequential, mask=ipv6_mask, start_ip=start_ipv6)
 
     with open(filepath_in, 'rb') as capfile:
 
-        #open cap file with pcapfile
+        # open cap file with pcapfile
         cap = savefile.load_savefile(capfile, verbose=False)
 
-        #use scapy's pcapwriter
+        # use scapy's pcapwriter
         pktwriter = PcapWriter(filepath_out, append=append)
 
         try:
             for pkt in cap.packets:
-                
-                #create scapy packet from pcapfile packet raw output
+
+                # create scapy packet from pcapfile packet raw output
                 new_pkt = Ether(pkt.raw())
 
-                #MAC addresses
+                # MAC addresses
                 try:
                     new_pkt.src = mac_gen.get_mac(new_pkt.src)
                     new_pkt.dst = mac_gen.get_mac(new_pkt.dst)
                 except:
                     pass
 
-                #VLAN number
-                if fixed_vlan: # not None
+                # VLAN number
+                if fixed_vlan:  # not None
                     try:
                         new_pkt['Dot1Q'].vlan = fixed_vlan
                     except:
                         pass
 
-                #IP Addresses
+                # IP Addresses
                 try:
                     new_pkt['IP'].src = ip4_gen.get_ip(new_pkt['IP'].src)
                     new_pkt['IP'].dst = ip4_gen.get_ip(new_pkt['IP'].dst)
@@ -272,7 +278,7 @@ def sanitize(filepath_in, *, filepath_out = None, sequential=True, append=False,
                 except IndexError:
                     pass
 
-                #sanitize ARP addresses
+                # sanitize ARP addresses
                 try:
                     new_pkt['ARP'].hwsrc = mac_gen.get_mac(new_pkt['ARP'].hwsrc)
                     new_pkt['ARP'].hwdst = mac_gen.get_mac(new_pkt['ARP'].hwdst)
@@ -281,7 +287,7 @@ def sanitize(filepath_in, *, filepath_out = None, sequential=True, append=False,
                 except IndexError:
                     pass
 
-                #fix checksum in each layer, starting at the top layer
+                # fix checksum in each layer, starting at the top layer
                 for layer in range(12, 0, -1):
                     try:
                         del new_pkt[layer].chksum
@@ -294,26 +300,36 @@ def sanitize(filepath_in, *, filepath_out = None, sequential=True, append=False,
             pktwriter.close()
 
     if info:
-        print('This file has %s IPv4/IPv6 endpoints and %s MAC endpoints' % (len(ip4_gen.mappings) + len(ip6_gen.mappings), len(mac_gen.mappings)))
+        print('This file has %s IPv4/IPv6 endpoints and %s MAC endpoints' % (
+            len(ip4_gen.mappings) + len(ip6_gen.mappings), len(mac_gen.mappings)))
         print('File created: %s' % filepath_out)
 
-#If run as a CLI util
+
+# If run as a CLI util
 if __name__ == '__main__':
-    import sys, argparse
+    import sys
+    import argparse
 
     parser = argparse.ArgumentParser()
     parser.add_argument("filepath_in", help="The pcap file to sanitize.")
     parser.add_argument("-o", "--filepath_out", default=None, help="File path to store the sanitized pcap.")
     parser.add_argument("-s", "--sequential", default=True, type=bool, help="Use sequential IPs/MACs in sanitization.")
-    parser.add_argument("-a", "--append", default=False, type=bool, help="Append to, instead of overwriting output file..")
-    parser.add_argument("--ipv4mask", default=0, type=int, help="Apply a mask to sanitized IPv4 addresses (Eg. mask of 8 preserves first octet).")
-    parser.add_argument("--ipv6mask", default=0, type=int, help="Apply a mask to sanitized IPv6 addresses (Eg. mask of 16 preserves first chazwazza).")
-    parser.add_argument("--macmask", default=0, type=int, help="Apply a mask to sanitized IPv6 addresses (Eg. mask of 24 preserves manufacturer).")
-    parser.add_argument("--startipv4", default='10.0.0.1', help="Start sequential IPv4 sanitization with this IPv4 addresses.")
-    parser.add_argument("--startipv6", default='2001:aa::1', help="Start sequential IPv6 sanitization with this IPv6 addresses.")
-    parser.add_argument("--startmac", default='00:aa:00:00:00:01', help="Start sequential MAC sanitization with this MAC addresses.")
+    parser.add_argument("-a", "--append", default=False, type=bool,
+                        help="Append to, instead of overwriting output file..")
+    parser.add_argument("--ipv4mask", default=0, type=int,
+                        help="Apply a mask to sanitized IPv4 addresses (Eg. mask of 8 preserves first octet).")
+    parser.add_argument("--ipv6mask", default=0, type=int,
+                        help="Apply a mask to sanitized IPv6 addresses (Eg. mask of 16 preserves first chazwazza).")
+    parser.add_argument("--macmask", default=0, type=int,
+                        help="Apply a mask to sanitized IPv6 addresses (Eg. mask of 24 preserves manufacturer).")
+    parser.add_argument("--startipv4", default='10.0.0.1',
+                        help="Start sequential IPv4 sanitization with this IPv4 addresses.")
+    parser.add_argument("--startipv6", default='2001:aa::1',
+                        help="Start sequential IPv6 sanitization with this IPv6 addresses.")
+    parser.add_argument("--startmac", default='00:aa:00:00:00:01',
+                        help="Start sequential MAC sanitization with this MAC addresses.")
     parser.add_argument("--fixedvlan", default=None, type=int, help="Overwrite VLANID (fixed)")
-    
+
     args = parser.parse_args()
 
     try:
